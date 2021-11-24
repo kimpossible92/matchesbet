accfad53a34ad4535022925ac406755aa9d5bb40
kimpossible92 <kim.possible92@bk.ru>
2021-11-25 01:22:04 +0300

    sql6

--- Assets/Cubs/PlayerTowerController.cs
+++ Assets/Cubs/PlayerTowerController.cs
@@ -19,25 +19,26 @@ public class PlayerTowerController : Cubs
         float JumpPower = 0.35f;
        
         float MaxJumpTime = 0.45f;
-        var isGround = Physics.Raycast(transform.position, Vector3.down, 1 * 2f, mask);
+        var isGround = Physics.Raycast(transform.position, Vector3.down, 1 * 0.6f, mask);
 
-        
+        Debug.DrawRay(transform.position, Vector3.down, Color.red,1*0.6f);
         if (isGround && (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)))
         {
             MaxJumpTime = _StoreMaxTime;
+            transform.Translate(new Vector3(0,1,0.72f) * 2.6f);
         }
-
-        if ((Input.GetKey(KeyCode.Space)||Input.GetMouseButton(0)) && MaxJumpTime > 0&& transform.position.y<=1.6f)
+        if (transform.position.y >= 1.8f) { MaxJumpTime = -1f; }
+        else if ((Input.GetKey(KeyCode.Space)||Input.GetMouseButton(0)) && MaxJumpTime > 0 && transform.position.y<=2f)
         {
-            MaxJumpTime -= Time.deltaTime;
-            transform.position += new Vector3(0, JumpPower, 0.1f);
-            //rb.AddForce(new Vector2(0, JumpPower), ForceMode.Impulse);
+            //MaxJumpTime -= Time.deltaTime;
+            //transform.position += new Vector3(0, JumpPower, 0.1f);
         }
 
         if ((Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) && !isGround)
         {
             MaxJumpTime = -1f;
         }
+       
     }
     private void jumpdelay()
     {
